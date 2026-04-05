319ba7f8c89b0c0ba6c781f85adf3bdc780bcc2e
Khalid Auwal <khalidauwal732@gmail.com>
2026-04-05 22:35:56 +0100

    chore: remove all debug, test, and documentation clutter files

--- backend/api/v2/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true,
-    'message' => 'Direct PHP file works!',
-    'note' => 'If you see this, Apache is working but .htaccess routing is not'
-]);
